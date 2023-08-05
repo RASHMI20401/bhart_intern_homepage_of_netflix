da7d7ca541dcc74c66aae5d6639729e8a93c9f77
RASHMI20401 <135140990+RASHMI20401@users.noreply.github.com>
2023-08-06 01:06:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+gsap.from(".features .row1", {
+    
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row1",
+        scroller: "body",
+        // markers: true,
+        start: "top 130%",
+    },
+    
+})
+gsap.from(".features .row2", {
+    
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row2",
+        scroller: "body",
+        // markers: true,
+        start: "top 130%",
+    },
+    
+})
+gsap.from(".features .row3", {
+    
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row3",
+        scroller: "body",
+        // markers: true,
+        start: "top 130%",
+    },
+    
+})
+gsap.from(".features .row4", {
+    
+    y:700,
+    scrollTrigger:{
+        trigger:".features .row4",
+        scroller: "body",
+        // markers: true,
+        start: "top 130%",
+    },
+    
+})
